b7dbd853a8d3fe7374ea0c978e0b4418ac7a21b0
Julien Blanchard <julien@sideburns.eu>
2017-02-12 14:50:12 +0100

    Lessons 8 to 10

--- Bingo.elm
+++ Bingo.elm
@@ -4,6 +4,38 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 
 
+-- MODEL
+
+
+type alias Model =
+    { name: String
+    , gameNumber: Int
+    , entries: List Entry
+    }
+
+type alias Entry = { id: Int
+                   , phrase: String
+                   , points: Int
+                   , marked: Bool
+                   }
+
+
+initialModel : Model
+initialModel =
+    { name = "Julien"
+    , gameNumber = 1
+    , entries = initialEntries
+    }
+
+initialEntries : List Entry
+initialEntries =
+    [ Entry 1 "Future-Proof" 100 False
+    , Entry 2 "Doing Agile" 200 False
+    ]
+
+
+--VIEW
+
 playerInfo : String -> Int -> String
 playerInfo name gameNumber =
     name ++ " - Game #" ++ (toString gameNumber)
@@ -36,14 +68,15 @@ viewFooter =
         ]
 
 
-view : Html msg
-view =
+view : Model -> Html msg
+view model =
     div [ class "content" ]
         [ viewHeader "BUZZWORD BINGO"
-        , viewPlayer "Julien" 3
+        , viewPlayer model.name model.gameNumber
+        , div [ class "debug" ] [ text (toString model) ]
         , viewFooter
         ]
 
 main : Html msg
 main =
-    view
+    view initialModel
